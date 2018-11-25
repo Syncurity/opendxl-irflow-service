f87c38454fc635a87a77a47bf66eb0922ed19ed3
punkrokk <jp@syncurity.net>
2018-11-25 13:59:22 -0500

    test conf.py for RTD

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'doc/sdk/index'
 
 # General information about the project.
 project = "IR-FLow Service"
